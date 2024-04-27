a3d08e8b614573b252565f5f651e2a9495802932
suman <developerasteroid@gmail.com>
2024-04-27 15:18:48 +0530

    added sql file

---
+++ panoravision360.sql
@@ -0,0 +1,142 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 27, 2024 at 11:34 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `panoravision360`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(4) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `name`, `username`, `password`) VALUES
+(1, 'Admin', 'Admin', '$2y$10$ElvuN1XrwxtiTiXrAXyf8uohceKVIav8zD1EVX9z02.X1iQaCj93K');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `images`
+--
+
+CREATE TABLE `images` (
+  `id` int(6) NOT NULL,
+  `name` varchar(60) NOT NULL,
+  `description` varchar(1000) NOT NULL,
+  `image` varchar(100) NOT NULL,
+  `position` int(10) DEFAULT NULL,
+  `updatedAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `images`
+--
+
+INSERT INTO `images` (`id`, `name`, `description`, `image`, `position`, `updatedAt`) VALUES
+(5, 'Food', 'big food pack   \r\n', 'IMG-662b8ccf7d1525.36755019.jpg', 1, '2024-04-26 11:53:50'),
+(6, 'Hall', 'big', 'IMG-662b8a8246f696.53329412.webp', 1, '2024-04-26 17:36:33'),
+(7, 'class', 'goo clas', 'IMG-662b8a9613d3f8.68454452.jpg', NULL, '2024-04-26 11:54:52'),
+(8, 'Library', 'libraryyyyy', 'IMG-662b8fbc9c8aa2.79732280.jpg', 2, '2024-04-26 12:18:17'),
+(9, 'office', 'offi', 'IMG-662b901e083646.80158596.jpg', NULL, '2024-04-26 11:29:34'),
+(10, 'last', 'dsadg', 'IMG-662b9b5261d1f4.22872391.jpeg', 2, '2024-04-26 12:18:08'),
+(11, 'my', 'fdds', 'IMG-662bf46c046997.64788002.jpg', 1, '2024-04-26 18:37:42'),
+(12, 'asd', 'asd', 'IMG-662cbd0eea8159.02589965.jpg', 1, '2024-04-27 08:53:47'),
+(13, 'asdfg', 'dfg', 'IMG-662cbe5ca53e99.41439267.jpg', 1, '2024-04-27 08:59:19');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(8) NOT NULL,
+  `name` varchar(60) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `name`, `email`, `password`) VALUES
+(1, 'suman', 'suman@gmail.com', '$2y$10$gyuM8uug32py0IeH1uJ0eetXSM4PqPnY.b3TvCaqYVO1NtPufgqpa'),
+(2, 'ashwith', 'ashwith@gmail.com', '$2y$10$9LRegpbyIKwxvsK0tQnHhO0R50jE7NUn.dApeZu2kqQ0q/FvKRNK.');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `images`
+--
+ALTER TABLE `images`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `images`
+--
+ALTER TABLE `images`
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
