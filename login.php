43cae00234d9816592945f589910a8b084f0af9a
suman <developerasteroid@gmail.com>
2024-05-11 11:29:45 +0530

    updated login and register to remove encryption

--- login.php
+++ login.php
@@ -1,52 +1,53 @@
 <?php
-    session_start();
-    if(isset($_SESSION['user'])){
-         // Redirect to the login page
-        header("Location: home.php");
-        exit();
-    }
-    $email_err ="";
-    $password_err = "";
-    $email = "";
-    $password = "";
-    if(isset($_POST['submit']) && isset($_POST['email']) && isset($_POST['password'])){
-        include('./config/db_connect.php');
-        $email = $_POST['email'];
-        $password = $_POST['password'];
+session_start();
+if (isset($_SESSION['user'])) {
+    // Redirect to the login page
+    header("Location: home.php");
+    exit();
+}
+$email_err = "";
+$password_err = "";
+$email = "";
+$password = "";
+if (isset($_POST['submit']) && isset($_POST['email']) && isset($_POST['password'])) {
+    include('./config/db_connect.php');
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 
-        $sql = "SELECT * FROM user WHERE email = ?";
-        $stmt = mysqli_prepare($conn, $sql);
+    $sql = "SELECT * FROM user WHERE email = ?";
+    $stmt = mysqli_prepare($conn, $sql);
 
-        mysqli_stmt_bind_param($stmt, "s", $email);
+    mysqli_stmt_bind_param($stmt, "s", $email);
 
-        mysqli_stmt_execute($stmt);
+    mysqli_stmt_execute($stmt);
 
-        $result = mysqli_stmt_get_result($stmt);
+    $result = mysqli_stmt_get_result($stmt);
 
-        if (mysqli_num_rows($result) > 0){
-            $row = mysqli_fetch_assoc($result);  
-            if (password_verify($password, $row['password'])) {
-                $_SESSION['user'] = true;
-                $_SESSION['user_name'] = $row['name'];
-                header("Location: home.php");
-                exit();
-            } else {
-                $password_err = "Incorrect password";
-            }
+    if (mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_assoc($result);
+        if ($password == $row['password']) {
+            $_SESSION['user'] = true;
+            $_SESSION['user_name'] = $row['name'];
+            header("Location: home.php");
+            exit();
         } else {
-            $email_err = "User with this email not found";
+            $password_err = "Incorrect password";
         }
-
+    } else {
+        $email_err = "User with this email not found";
     }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <link rel="stylesheet" href="./css/login.css">
 </head>
+
 <body>
     <form action="" method="post">
         <h1>Login</h1>
@@ -60,4 +61,5 @@
         <p class="create-new-txt">Dont have a Account? <a href="register.php">create new</a></p>
     </form>
 </body>
+
 </html>
--- register.php
+++ register.php
@@ -1,57 +1,58 @@
 <?php
-    $name = "";
-    $email = "";
-    $password = "";
-    $email_err = "";
-    $password_err = "";
-    
-    if(isset($_POST['submit']) && isset($_POST['name']) && isset($_POST['email']) && isset($_POST['password']) && isset($_POST['confirmPassword'])){
-        $name = $_POST['name'];
-        $email = $_POST['email'];
-        $password = $_POST['password'];
-        if($_POST['confirmPassword'] != $password){
-            $password_err = "Password do not match";
+$name = "";
+$email = "";
+$password = "";
+$email_err = "";
+$password_err = "";
+
+if (isset($_POST['submit']) && isset($_POST['name']) && isset($_POST['email']) && isset($_POST['password']) && isset($_POST['confirmPassword'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    if ($_POST['confirmPassword'] != $password) {
+        $password_err = "Password do not match";
+    } else {
+        include('./config/db_connect.php');
+        $sql = "SELECT * FROM user WHERE email = ?";
+        $stmt = mysqli_prepare($conn, $sql);
+
+        mysqli_stmt_bind_param($stmt, "s", $email);
+
+        mysqli_stmt_execute($stmt);
+
+        $result = mysqli_stmt_get_result($stmt);
+        mysqli_stmt_close($stmt);
+        if (mysqli_num_rows($result) > 0) {
+            $email_err = "Email already in use. Please try another.";
         } else {
-            include('./config/db_connect.php');
-            $sql = "SELECT * FROM user WHERE email = ?";
+            $sql = "INSERT INTO user (name, email, password) VALUES (?, ?, ?)";
             $stmt = mysqli_prepare($conn, $sql);
-
-            mysqli_stmt_bind_param($stmt, "s", $email);
-
-            mysqli_stmt_execute($stmt);
-
-            $result = mysqli_stmt_get_result($stmt);
-            mysqli_stmt_close($stmt);
-            if (mysqli_num_rows($result) > 0){
-                $email_err = "Email already in use. Please try another.";
+            $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+            mysqli_stmt_bind_param($stmt, "sss", $name, $email, $password);
+            if (mysqli_stmt_execute($stmt)) {
+                echo "<script>alert('Registered Successfully')</script>";
+                echo "<script>window.location.href = 'login.php';</script>";
+                exit();
             } else {
-                $sql = "INSERT INTO user (name, email, password) VALUES (?, ?, ?)";
-                $stmt = mysqli_prepare($conn, $sql);
-                $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-                mysqli_stmt_bind_param($stmt, "sss", $name, $email, $hashed_password);
-                if (mysqli_stmt_execute($stmt)) {
-                    echo "<script>alert('Registered Successfully')</script>";
-                    echo "<script>window.location.href = 'login.php';</script>";
-                    exit();
-                  } else {
-                    echo "<script>alert('Error:  Failed to register')</script>";
-                  }
+                echo "<script>alert('Error:  Failed to register')</script>";
             }
-
         }
     }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register</title>
     <link rel="stylesheet" href="./css/register.css">
 </head>
+
 <body>
     <form action="" method="post">
-    <h1>Register</h1>
+        <h1>Register</h1>
         <label for="name">Name</label>
         <input type="text" id="name" name="name" placeholder="Name" required value="<?php echo $name ?>">
         <label for="email">Email</label>
@@ -66,4 +67,5 @@
         <p class="already-have-txt">Already have a Account? <a href="login.php">login</a></p>
     </form>
 </body>
+
 </html>
